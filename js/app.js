87519e37d4c24873d882d3109b6d26f1fcf2964c
caranuial <caranuial@gmail.com>
2015-10-20 23:05:15 +0200

    List Works
    List Search working

--- js/app.js
+++ js/app.js
@@ -18,37 +18,17 @@ function point(data) {
         wikiPage: self.wikiPage
     });
 
-    //console.log (marker.altNames());
-
-    /*google.maps.event.addListener(marker, 'click', (function(marker){
-            return function() {
-                toggleBounce(marker);
-            }           
-        })(marker)
-    );*/
-
     marker.addListener('click', (function(marker){
             return function() {
                 toggleBounce(marker);
                 vm.searchForWikiPage(marker);
-                //vm.disableMarkers(marker);
+                vm.disableMarkers(marker);
+                var latLng = marker.getPosition();
+                infoWindow.setContent('<h4>Castle position is:</h4>' + latLng.lat() + ', ' + latLng.lng());
+                infoWindow.open(map, marker);
             }           
         })(marker));
-    //    marker.addListener('click', viewModel.setSearchedPoint(this));
     markers.push(marker);
-    //if you need the poition while dragging
-    /*google.maps.event.addListener(marker, 'drag', function() {
-        var pos = marker.getPosition();
-        this.lat(pos.lat());
-        this.long(pos.lng());
-    }.bind(this));
-
-    //if you just need to update it when the user is done dragging
-    google.maps.event.addListener(marker, 'dragend', function() {
-        var pos = marker.getPosition();
-        this.lat(pos.lat());
-        this.long(pos.lng());
-    }.bind(this));*/
 };
 
 function toggleBounce(marker) {
@@ -60,7 +40,7 @@ function toggleBounce(marker) {
 
 
 var width = $(window).width();
-//console.log (width);
+
 var zoomLevel;
 if (width > 1080){
     zoomLevel = 8;
@@ -76,7 +56,7 @@ var map =  new google.maps.Map(document.getElementById('map'), {
     mapTypeId: google.maps.MapTypeId.ROADMAP
 });
 
-
+var infoWindow = new google.maps.InfoWindow();
 
 var getCen = map.getCenter();
 
@@ -89,10 +69,11 @@ google.maps.event.addDomListener(window, 'resize', function() {
     } else {
         map.setZoom(6);
     }
-
-    
 });
 
+google.maps.event.addListener(map, 'click', function() {
+    infoWindow.close();
+});
 
 
 var markers =[];
@@ -127,8 +108,8 @@ var pointsOfInterest = [
                 {nick: 'Nitriansky hrad'},
                 {nick: 'Nitra Castle'}
             ],
-            lat: 48.19,
-            lng: 18.05,
+            lat: 48.32,
+            lng: 18.09,
             wikiPage: 'Nitra_Castle'
         },
         {
@@ -142,8 +123,8 @@ var pointsOfInterest = [
                 {nick: 'Strečno Castle'},
                 {nick: 'Strecno Castle'}
             ],
-            lat: 49.10,
-            lng: 18.51,
+            lat: 49.16,
+            lng: 18.88,
             wikiPage: 'Strečno_Castle'
         },
         {
@@ -156,8 +137,8 @@ var pointsOfInterest = [
                 {nick: 'Tematín Castle'},
                 {nick: 'Tematin Castle'}
             ],
-            lat: 48.40,
-            lng: 17.55,
+            lat: 48.68,
+            lng: 17.93,
             wikiPage: 'Tematín'
         },
         {
@@ -168,8 +149,8 @@ var pointsOfInterest = [
                 {nick: 'Trenčín Castle'},
                 {nick: 'Trencin Castle'}
             ],
-            lat: 48.53,
-            lng: 18.02,
+            lat: 48.88,
+            lng: 18.04,
             wikiPage: 'Trenčín_Castle'
         },
         {
@@ -192,8 +173,8 @@ var pointsOfInterest = [
                 {nick: 'Krásna Hôrka Castle'},
                 {nick: 'Krasna Horka Castle'}               
             ],
-            lat: 48.39,
-            lng: 20.36,
+            lat: 48.66,
+            lng: 20.60,
             wikiPage: 'Krásna_Hôrka_Castle'
         },
         {
@@ -204,8 +185,8 @@ var pointsOfInterest = [
                 {nick: 'Šariš Castle'},
                 {nick: 'Saris Castle'}
             ],
-            lat: 49.03,
-            lng: 21.10,
+            lat: 49.05,
+            lng: 21.17,
             wikiPage: 'Šariš_Castle'
         },
         {
@@ -215,8 +196,8 @@ var pointsOfInterest = [
                 {nick: 'Bojnicky zamok'},
                 {nick: 'Bojnice Castle'},
             ],
-            lat: 48.46,
-            lng: 18.34,
+            lat: 48.79,
+            lng: 18.56,
             wikiPage: 'Bojnice_Castle'
         }
 
@@ -228,10 +209,23 @@ var viewModel = function() {
 
     this.points = ko.observableArray([]);
 
+    this.pointsList = ko.observableArray([]);
+
     pointsOfInterest.forEach(function(data){
         
         self.points.push(new point(data));
     });
+
+    this.fillPointsList = function(){
+        
+        self.pointsList.removeAll();
+
+        this.points().forEach(function(data) {
+            self.pointsList.push(data);
+        });    
+    }
+
+    this.fillPointsList();
 
     self.searchedPoint = ko.observable("");  
 
@@ -252,7 +246,7 @@ var viewModel = function() {
     }
 
     this.setDefaultWikiFields = function(){
-        self.setWikiFields("Please serach for castle or select one from map or pick one from menu. <br> To reset search click <i class='glyphicon glyphicon-repeat'></i>. ", "Slovak Castles", "", "");
+        self.setWikiFields("Please serach for castle or select one from map or pick one from menu. <br> To reset search click <i class='glyphicon glyphicon-repeat'></i> or selected castle again. ", "Slovak Castles", "", "");
     }
 
     this.setDefaultWikiFields();
@@ -266,14 +260,21 @@ var viewModel = function() {
         var visibility = false;
         var amountOfPositiveSearches;
 
-        /*ko.utils.arrayForEach(self.points(), function(obj) {
-            ko.utils.arrayForEach(obj.altNames(), function(altName){
+        self.pointsList.removeAll();
+
+        self.points().forEach(function(obj) {
+            
+            obj.altNames().forEach(function(altName){
                 if(altName.nick.toLowerCase().indexOf(search) >= 0){
-                    //console.dir(altName.nick);
-                };    
+                    console.log(altName.nick + "True");
+                    searchSelf.visibility = true; 
+                } 
             });
-            /// tu pracovat na prehladavani hradov na zaklade nickov a filtrovat markre
-        });*/
+            if (searchSelf.visibility === true) {
+                self.pointsList.push(obj);
+            };
+            searchSelf.visibility = false;
+        });
 
         this.amountOfPositiveSearches = 0;
 
@@ -319,6 +320,26 @@ var viewModel = function() {
 
     };
 
+    this.selectListPoint = function(point){
+        if (self.pointsList().length != 1 ){
+            self.pointsList.removeAll();
+            self.points().forEach(function(obj) {
+                if(obj.name().toLowerCase().indexOf(point.name().toLowerCase()) >= 0){
+                    self.pointsList.push(obj);
+                } 
+            });
+            
+            markers.forEach(function(marker){
+                if (marker.title.toLowerCase() === point.name().toLowerCase()){
+                    self.disableMarkers(marker);
+                    self.searchForWikiPage(marker);
+                }
+            });  
+        } else {
+            self.resetSearch();
+        } 
+    }
+
     this.disableMarkers = function(marker){
 
         ko.utils.arrayForEach(markers, function(searchedMarker) {
@@ -342,8 +363,8 @@ var viewModel = function() {
         self.enableMarkers();
         self.setDefaultWikiFields();
         self.setSearchedPoint("");
-        
-
+        infoWindow.close();
+        self.fillPointsList();
     }
 
     this.searchForWikiPage = function(marker){
